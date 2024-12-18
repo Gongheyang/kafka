82a022138206403fb4a79797355e964e1d74aacf
Lucas Brutschy <lbrutschy@confluent.io>
2024-12-18 15:52:39 +0100

    fix

--- core/src/test/scala/unit/kafka/server/RequestQuotaTest.scala
+++ core/src/test/scala/unit/kafka/server/RequestQuotaTest.scala
@@ -746,9 +746,6 @@ class RequestQuotaTest extends BaseRequestTest {
         case ApiKeys.STREAMS_GROUP_DESCRIBE =>
           new StreamsGroupDescribeRequest.Builder(new StreamsGroupDescribeRequestData(), true)
 
-        case ApiKeys.STREAMS_GROUP_HEARTBEAT =>
-          new StreamsGroupHeartbeatRequest.Builder(new StreamsGroupHeartbeatRequestData(), true)
-
         case _ =>
           throw new IllegalArgumentException("Unsupported API key " + apiKey)
     }

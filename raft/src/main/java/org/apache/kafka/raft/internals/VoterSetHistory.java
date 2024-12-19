2aa4767723b942d6c48cdce216651efdf93050fd
nihyxx <nihyxx@gmail.com>
2024-12-18 16:46:27 -0800

    Fix import

--- raft/src/main/java/org/apache/kafka/raft/internals/VoterSetHistory.java
+++ raft/src/main/java/org/apache/kafka/raft/internals/VoterSetHistory.java
@@ -18,6 +18,7 @@ package org.apache.kafka.raft.internals;
 
 import org.apache.kafka.common.utils.LogContext;
 import org.apache.kafka.raft.VoterSet;
+
 import org.slf4j.Logger;
 
 import java.util.Optional;
